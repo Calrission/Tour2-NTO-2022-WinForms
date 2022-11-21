c6f667cade90307ee4bbf4b1d33e4b9887aaa8f6
Kavrese <kavrese@gmail.com>
2022-11-21 11:35:43 +0300

    добавил релиз debug версии

--- EditHotel.cs
+++ EditHotel.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.ComponentModel;
 using System.Text.RegularExpressions;
 
